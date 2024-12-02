a23b710f4e12176def1f92955c2805868ee90650
OsvaldSchierbeck <os@organicgrowth.dk>
2024-12-02 08:54:15 +0100

    initial round of changes colors with etc

--- s-v-tool/tailwind.config.js
+++ s-v-tool/tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primaryTeal: "#00A2AD",
+        darkerTeal: "#014656",
+        orangeTeal: "#FDD65D",
+        darkerOrange: "#F6A801",
+        svBg: "#F9F7F6",
+        svText: "#1C1D26",
+        iconGray: "#B6B6B6",
+      },
+      fontFamily: {
+        GeistMono: ["GeistMono", "sans-serif"],
+        GeistVF: ["GeistVF", "sans-serif"],
       },
     },
   },
