76706439585885c59213f5c5f1fe6bd97f4d59e9
OsvaldSchierbeck <os@organicgrowth.dk>
2024-12-03 13:34:33 +0100

    expandable venture type

--- s-v-tool/src/app/comps/toolComps/VentureDropdown.jsx
+++ s-v-tool/src/app/comps/toolComps/VentureDropdown.jsx
@@ -19,7 +19,11 @@ export default function VentureDropDown({
         <div className="w-full h-full rounded-md bg-darkerOrange flex justify-center">
           <h3 className="text-center ml-4">{selectedVenture}</h3>
         </div>
-        <div className="min-w-[38px] h-full rounded-md bg-primaryTeal flex rotate-[0deg]">
+        <div
+          className={`min-w-[38px] h-full rounded-md bg-primaryTeal flex transition-transform ${
+            !isExpanded ? "" : "rotate-[180deg]"
+          }`}
+        >
           <div className="h-[22px] w-[28px] my-auto mx-auto overflow-clip relative rounded-sm ">
             <img
               src="./icons/whiteArrow.svg"
@@ -30,22 +34,29 @@ export default function VentureDropDown({
         </div>
       </div>
       <div
-        className={`absolute top-[38px] w-[365px] ${
-          !isExpanded ? styles.collapsed : styles.expanded
+        className={`absolute top-[38px] w-[365px] overflow-clip ${
+          !isExpanded ? styles.collapsed : ""
         }`}
       >
-        {ventureArray.map((venture, index) => (
-          <div
-            key={index}
-            onClick={() => {
-              setSelectedVenture(venture);
-              setIsExpanded((prev) => !prev);
-            }}
-          >
-            <p className="text-center my-2 ">{venture}</p>
-            <div className="w-3/4 h-[2px] mx-auto bg-darkerOrange" />
-          </div>
-        ))}
+        <div
+          className={`${
+            !isExpanded ? styles.translateCollapsed : styles.translateExpand
+          }`}
+        >
+          {ventureArray.map((venture, index) => (
+            <div
+              key={index}
+              onClick={() => {
+                setSelectedVenture(venture);
+                setIsExpanded((prev) => !prev);
+              }}
+              className="bg-svBg"
+            >
+              <p className="text-center my-2 ">{venture}</p>
+              <div className="w-3/4 h-[2px] mx-auto bg-darkerOrange" />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
